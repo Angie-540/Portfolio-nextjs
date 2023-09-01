28e18311fd8e26f35d241353eff2dbceaef6d33a
Angie-540 <janiceangella21@gmail.com>
2023-09-01 09:35:05 +0300

    about page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import NavBar from '@/components/Navbar';
 import '@/styles/globals.css';
 import {Montserrat} from "next/font/google";
 import Head from "next/head";
+import Footer from '@/components/Footer';
 
 const montserrat = Montserrat ({
   subsets: ["latin"],
@@ -19,6 +20,7 @@ export default function App({ Component, pageProps }) {
     <main className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
       <NavBar />
       <Component {...pageProps} />
+      <Footer />
     </main>
     </>
   );
