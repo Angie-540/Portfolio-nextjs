42d13d3320f2c1f4369a3cb570d2ad76e6ee3480
Angie-540 <janiceangella21@gmail.com>
2023-09-04 10:57:46 +0300

    dark theme

--- src/pages/about.js
+++ src/pages/about.js
@@ -48,7 +48,7 @@ const about = () => {
             <title>CodeBucks | About  Page</title>
             <meta name="description" context="any description" />
         </Head>
-        <main className='flex w-full flex-col items-center justify-center'>
+        <main className='flex w-full flex-col items-center justify-center dark:text-light '>
             <Layout className='pt-16'>
             <AnimatedText text="Passion Fuels Purpose" className='mb-16' /> 
             <div className='grid w-full grid-cols-8 gap-16'>
@@ -78,7 +78,7 @@ every project I work on. I look forward to the opportunity to bring my skills an
                 <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark
                 bg-light p-8' >
 
-                <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark ' />
+                <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light' />
                     <Image src={profilePic} alt="Codebucks" className='w-full h-auto rounded-2xl'/>
                 </div>
 
@@ -88,19 +88,20 @@ every project I work on. I look forward to the opportunity to bring my skills an
                         <span className='inline-block text-7xl font-bold'  >
                             <AnimatedNumbers value={50} />+
                         </span>
-                        <h2 className='text-xl font-medium capitalize text-dark/75' >satisfied clients</h2>
+                        <h2 
+                        className='text-xl font-medium capitalize text-dark/75 dark dark:text-light/75' >satisfied clients</h2>
                     </div>
                     <div className='flex flex-col items-end justify-center' >
                         <span className='inline-block text-7xl font-bold' >
                             <AnimatedNumbers value={40} /> +
                         </span>
-                        <h2 className='text-xl font-medium capitalize text-dark/75' >projects completed</h2>
+                        <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75' >projects completed</h2>
                     </div>
                     <div className='flex flex-col items-end justify-center' >
                         <span className='inline-block text-7xl font-bold' >
                         <AnimatedNumbers value={4} /> +
                         </span>
-                        <h2 className='text-xl font-medium capitalize text-dark/75' >years of experience</h2>
+                        <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75' >years of experience</h2>
                     </div>
                 </div>
 
